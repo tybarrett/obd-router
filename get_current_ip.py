ad654246695f132ce2384a1c7783f84ac87c33cb
Tyler <tylerbarrett2010+github@gmail.com>
2025-11-21 18:36:46 -0500

    Better way of finding current ip

--- get_current_ip.py
+++ get_current_ip.py
@@ -2,11 +2,7 @@ import os
 import re
 
 def get_current_ip():
-    output = os.popen("ifconfig").read()
-    all_ip_matches = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", output, flags=0)
-    for ip in all_ip_matches:
-        if ip.startswith("10.241"):
-            return ip
+    return os.popen("ifconfig wlan0 | grep 'inet ' | cut -d \" \" -f 10").read()
 
 if __name__ == "__main__":
     print(get_current_ip())
